fce32d949e6cd1a0f80b164d987cc90046726b34
kanghyew0n <104333249+kanghyew0n@users.noreply.github.com>
2024-08-26 22:20:49 +0900

    ✅ Update: GridSizeControl & ImageUploader 컴포넌트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,19 @@ import styled from "@emotion/styled";
 import Grid from "./components/Grid";
 import Header from "./components/Header";
 import ColorPicker from "./components/ColorPicker";
+import GridSizeControl from "./components/GridSizeControl";
+import ImageUploader from "./components/ImageUploader";
 
 function App() {
   return (
     <>
       <Header />
       <Body>
-        <ColorPicker />
-        <Grid column={50} row={30} />
+        <GridControlSection>
+          <GridSizeControl />
+          <ImageUploader />
+        </GridControlSection>
+        <Grid column={40} row={20} />
         <ColorPicker />
       </Body>
     </>
@@ -21,6 +26,13 @@ const Body = styled.main`
   padding: 20px;
   display: flex;
   justify-content: space-between;
+`;
+
+const GridControlSection = styled.section`
+  height: 100%;
+  > div + div {
+    margin-top: 20px;
+  }
 `;
 
 export default App;
