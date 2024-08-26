a324269ea34ecb5cf896df5012934533d802cf63
kanghyew0n <104333249+kanghyew0n@users.noreply.github.com>
2024-08-26 22:19:19 +0900

    ✨ Feat: ImageUploader 컴포넌트 생성

---
+++ src/components/ImageUploader.tsx
@@ -0,0 +1,37 @@
+import styled from "@emotion/styled";
+
+const ImageUploader = () => {
+  return (
+    <StyledImageUploader>
+      <ImageUploaderWrapper>+ 이미지 추가</ImageUploaderWrapper>
+      <span>이미지를 추가해보세요</span>
+      <span>격자 위에 이미지가 오버레이 돼요!</span>
+    </StyledImageUploader>
+  );
+};
+
+const StyledImageUploader = styled.div`
+  width: 150px;
+
+  span {
+    display: block;
+    font-size: 10px;
+    line-height: 130%;
+  }
+`;
+
+const ImageUploaderWrapper = styled.div`
+  width: 150px;
+  height: 110px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 10px;
+
+  font-size: 14px;
+  border-radius: 5px;
+  border: 1px solid #000;
+  background-color: #eee;
+`;
+
+export default ImageUploader;
