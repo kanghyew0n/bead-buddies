694dec5d0fa33d43f84f3fafa896a204a457878d
kanghyew0n <104333249+kanghyew0n@users.noreply.github.com>
2024-08-26 23:37:03 +0900

    ♻️ Refactor: grid 함수 column / row 변경시에만 적용되도록 수정

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,7 +1,12 @@
 import styled from "@emotion/styled";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
+import useGridSizeStore from "../store/useGridSizeStore";
+import { commonFlexCenter } from "../assets/styles/common-style";
 
 const HEADER_HEIGHT = 70;
+const WIDTH_PADDING = 500; // 250 * 2
+const HEIGHT_PADDING = 110; // 55 * 2
+const GRID_PADDING = 40; // 20 * 2
 
 interface GridProps {
   column: number;
@@ -12,28 +17,23 @@ interface GridItemProps {
   cellSize: number;
 }
 
-const Grid: React.FC<GridProps> = ({ column, row }) => {
+const Grid = () => {
+  const { column, row } = useGridSizeStore();
   const [cellSize, setCellSize] = useState(0);
 
-  const calcCellSize = () => {
-    if (column > row) {
-      const viewPortWidth = window.innerWidth;
-      const maxGridWidth = viewPortWidth - 460; // 250*2에서 padding 40 제외
-      const cellSize = Math.floor(maxGridWidth / column);
-      return cellSize;
-    }
-
-    if (column <= row) {
-      const viewPortHeight = window.innerHeight;
-      const maxGridHeight = viewPortHeight - HEADER_HEIGHT - 110;
-      const cellSize = Math.floor(maxGridHeight / row);
-      return cellSize;
-    }
-  };
+  const calcCellSize = useMemo(() => {
+    const viewPortWidth = window.innerWidth;
+    const viewPortHeight = window.innerHeight;
+    const maxGridWidth = viewPortWidth - WIDTH_PADDING - GRID_PADDING;
+    const maxGridHeight = viewPortHeight - HEADER_HEIGHT - HEIGHT_PADDING;
+  
+    return column > row
+      ? Math.floor(maxGridWidth / column)
+      : Math.floor(maxGridHeight / row);
+  }, [column, row]);
 
   useEffect(() => {
-    const _size = calcCellSize();
-    setCellSize(_size);
+    setCellSize(calcCellSize);
   }, [column, row]);
 
   return (
@@ -48,29 +48,28 @@ const Grid: React.FC<GridProps> = ({ column, row }) => {
 };
 
 const GridWrapper = styled.div`
-  height: calc(100vh - 70px - 40px); // header + T.B padding
-  display: flex;
-  align-items: center;
-  justify-content: center;
+  height: calc(
+    100vh - ${HEADER_HEIGHT}px - ${GRID_PADDING}px
+  ); // header + T.B padding
+  ${commonFlexCenter}
 `;
 
 const GridGuide = styled.div<GridProps>`
-  max-width: calc(100vw - 460px);
-  max-height: calc(100vh - 70px - 100px);
+  max-width: calc(100vw - ${WIDTH_PADDING}px);
+  max-height: calc(100vh - ${HEADER_HEIGHT}px - ${HEIGHT_PADDING}px);
   width: fit-content;
   height: fit-content;
   display: grid;
   grid-template-columns: ${({ column }) => `repeat(${column}, 1fr)`};
   grid-template-rows: ${({ row }) => `repeat(${row}, 1fr)`};
-  box-shadow: rgba(0,0,0,0.1) 10px 10px 20px;
-
+  box-shadow: rgba(0, 0, 0, 0.1) 10px 10px 20px;
 `;
 
-const GridItem = styled.div<GridItemProps>`
+const GridItem = React.memo(styled.div<GridItemProps>`
   width: ${({ cellSize }) => `${cellSize}px`};
   height: ${({ cellSize }) => `${cellSize}px`};
   border: 1px solid #ccc;
   background-color: #fff;
-`;
+`);
 
 export default Grid;
