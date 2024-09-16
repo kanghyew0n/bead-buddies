c82574c686feba8356f728464b7cd1fdfac18f2b
kanghyew0n <104333249+kanghyew0n@users.noreply.github.com>
2024-09-17 00:34:31 +0900

    ✨ Feat: theme추가

---
+++ src/styles/theme.ts
@@ -0,0 +1,31 @@
+export const theme = {
+  colors: {
+    primary: {
+      main: "#C8FF6F",
+      100: "#E0FFAE",
+      200: "#B4FF3B",
+    },
+    gray: {
+      100: "#F7F7F7",
+      200: "#cfcfcf",
+      300: "#eeeeee",
+      400: "#959595",
+    },
+    neutral: {
+      white: "#ffffff",
+      black: "#000000",
+    },
+  },
+  fontSizes: {
+    base: "14px",
+    sm: "12px",
+    lg: "20px",
+  },
+  spacing: {
+    sm: "5px",
+    md: "10px",
+    lg: "20px",
+  },
+};
+
+export default theme;
